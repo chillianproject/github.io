710cf115c33373556b1659877a4ba8848e46eb48
julwrites <julian.teh@outlook.com>
2020-08-23 17:13:14 +0800

    Adding Apps container and a Decision box

---
+++ src/components/Decision.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="decision">
+    <h2>Decision Maker</h2>
+    <br />Options
+  </div>
+</template>
+
+
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+
+@Component
+export default class Decision extends Vue {}
+</script>
